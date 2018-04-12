4537f214cdf809c7ebcccad2fe7855bcc28af3c0
Muataz Khataan <muataz.khataan@gmail.com>
2018-04-12 20:19:56 +0300

    First

---
+++ Assets/Characters/Maria/mariaControls.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mariaControls : MonoBehaviour {
+
+	static Animator anim;
+	//int isAttacking = Animator.StringToHash("isAttacking");
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Z))
+		{
+			print("isAttacking");
+			anim.SetTrigger("isAttacking");
+
+		}
+	}
+}
